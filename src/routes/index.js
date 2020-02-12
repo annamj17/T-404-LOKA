8d72d638549d9cb652459cc293ddb628f32f8832
annamj <annamj17@ru.is>
2020-02-12 11:35:43 +0000

    redux and navigation packages setup

---
+++ src/routes/index.js
@@ -0,0 +1,26 @@
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+
+import LoginView from '../views/LoginView/LoginView';
+
+const StackNavigator = createStackNavigator(
+  {
+    LoginView
+    // Login: { screen: LoginView, navigationOptions: { headerShown: false } }
+  },
+  {
+    defaultNavigationOptions: {
+      headerStyle: {
+        height: 80,
+        backgroundColor: '#087E8B',
+        elevation: 0, // Remove shadow from android
+        shadowOpacity: 0, // Remove shadow from ios
+        borderBottomWidth: 0
+      },
+      headerBackTitle: 'back',
+      headerTintColor: '#F9F9F9'
+    }
+  }
+);
+
+export default createAppContainer(StackNavigator);
