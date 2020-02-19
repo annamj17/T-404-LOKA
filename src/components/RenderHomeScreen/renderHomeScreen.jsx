3f8d66972957fb316a4a87883c129b38291b8aff
Margrét Vala Björgvinsdóttir <margretvalabjorgvinsdottir@Margrets-MacBook-Pro.local>
2020-02-19 10:34:47 +0000

    fixing for eslint

--- src/components/RenderHomeScreen/renderHomeScreen.jsx
+++ src/components/RenderHomeScreen/renderHomeScreen.jsx
@@ -1,17 +1,17 @@
 import React from 'react';
-import { View, Text, Image, TextInput, TouchableOpacity } from 'react-native';
-
-import styles from '../RenderLogin/styles';
-
+import { View, Text } from 'react-native';
 
 class RenderHomeScreen extends React.Component {
+  static navigationOptions = {
+    headerShown: false
+  };
 
-    render() {
-        return (
-            <View style={styles.container}>
-        
-            </View>
-        )
-    }
+  render() {
+    return (
+      <View>
+        <Text>Hallo</Text>
+      </View>
+    );
+  }
 }
 export default RenderHomeScreen;
