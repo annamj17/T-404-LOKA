db6e8f158276f52f55e5585c82f30bcd1eb3c962
annamj <annamj17@ru.is>
2020-02-19 11:17:27 +0000

    refactoring components

--- src/components/RenderHomeScreen/renderHomeScreen.jsx
+++ src/components/RenderHomeScreen/renderHomeScreen.jsx
@@ -1,19 +1,12 @@
 import React from 'react';
 import { View, Text } from 'react-native';
 
-import styles from '../RenderLogin/styles';
+const RenderHomeScreen = () => {
+  return (
+    <View>
+      <Text>HomeScreen</Text>
+    </View>
+  );
+};
 
-class RenderHomeScreen extends React.Component {
-  static navigationOptions = {
-    headerShown: false
-  };
-
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>HomeScreen</Text>
-      </View>
-    );
-  }
-}
 export default RenderHomeScreen;
--- src/components/RenderLogin/renderLogin.jsx
+++ src/components/RenderLogin/renderLogin.jsx
@@ -5,42 +5,32 @@ import styles from './styles';
 import logoImage from '../../../assets/K3imaginelogo.png';
 import bottomImage from '../../../assets/Capture.png';
 
-class RenderLogin extends React.Component {
-  static navigationOptions = {
-    headerShown: false
-  };
-
-  render() {
-    return (
-      <View style={styles.container}>
-        <Image
-          style={styles.logoImage}
-          source={logoImage}
-          resizeMode="contain"
-        />
-        <Text style={styles.loginHeader}>
-          Welcome, please log in to your account
-        </Text>
-        <View style={styles.inputView}>
-          <TextInput style={styles.inputText} placeholder="Username *" />
-        </View>
-        <View style={styles.inputView}>
-          <TextInput style={styles.inputText} placeholder="Password" />
-        </View>
-        <TouchableOpacity>
-          <Text style={styles.forgotPassword}>Forgot Password?</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.btn}>
-          <Text> Login </Text>
-        </TouchableOpacity>
-        <Image
-          style={styles.bottomImage}
-          source={bottomImage}
-          resizeMode="cover"
-        />
+const RenderLogin = () => {
+  return (
+    <View style={styles.container}>
+      <Image style={styles.logoImage} source={logoImage} resizeMode="contain" />
+      <Text style={styles.loginHeader}>
+        Welcome, please log in to your account
+      </Text>
+      <View style={styles.inputView}>
+        <TextInput style={styles.inputText} placeholder="Username *" />
       </View>
-    );
-  }
-}
+      <View style={styles.inputView}>
+        <TextInput style={styles.inputText} placeholder="Password *" />
+      </View>
+      <TouchableOpacity>
+        <Text style={styles.forgotPassword}>Forgot Password?</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.btn}>
+        <Text> Login </Text>
+      </TouchableOpacity>
+      <Image
+        style={styles.bottomImage}
+        source={bottomImage}
+        resizeMode="cover"
+      />
+    </View>
+  );
+};
 
 export default RenderLogin;
